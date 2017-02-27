0d8f5baba57226f197905d31795ca0cc56443ddb
Armin Blaze <box_for_spam@mail.ru>
2017-02-27 23:30:58 +0300

    Google map (standart version)

---
+++ js/map.js
@@ -0,0 +1,44 @@
+function initMap() {
+	var mapDiv = document.getElementById('map');
+				
+	var map = new google.maps.Map(mapDiv, {
+//          center: {lat: 59.941, lng: 30.2996},
+		center: new google.maps.LatLng(59.941, 30.2996),
+          zoom: 15,
+					scrollwheel: false,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+				
+				
+				var image = {
+				url: 'img/map__bullet.svg',
+				// This marker is 27 pixels wide by 27 pixels high.
+				size: new google.maps.Size(50, 50),
+				// The origin for this image is (0, 0).
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(14, 14)
+			};
+	
+	
+	/////
+			
+/////
+				
+				var myLatLng = new google.maps.LatLng(59.9389463, 30.3150017);
+	
+  			var beachMarker = new google.maps.Marker({
+    		position: myLatLng,
+   			map: map,
+//				draggable: true,
+   			icon: image
+  			});
+	
+//					var marker = new google.maps.Marker({
+//            position: myLatLng,
+//            map: map,
+//            draggable: true,
+//            icon: "img/map__bullet.svg"
+//        });
+      }
+			
+				google.maps.event.addDomListener(window, 'load', initMap);
